843fb16ac299d5045dd32e219c03e258d0e60ae7
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2021-03-24 16:35:22 +0100

    update config.py

--- app_config/config.py
+++ app_config/config.py
@@ -1,6 +1,6 @@
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:copycat@localhost:5432/test_db"   # replace with yours
+    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost:5432/test_db"   # replace with yours
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '\xf0?a\x9a\\\xff\xd4;\x0c\xcbHi'
     CSRF_ENABLED = True
